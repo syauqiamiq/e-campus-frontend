01a8bfea1c40d8b0a1d6163a3ab25388c02a2586
syauqiamiq <syauqiamiq12@gmail.com>
2023-10-10 23:41:33 +0700

    change button navbar

--- src/components/button/CustomButton.jsx
+++ src/components/button/CustomButton.jsx
@@ -5,7 +5,7 @@ const CustomButton = ({ title, type }) => {
 	return type == "outlined" ? (
 		<Button
 			variant="contained"
-			className="text-campus-blue rounded-full font-bold text-[14px] w-full h-50 hover:text-white shadow-none  !border !border-gray-300 border-solid"
+			className="text-campus-blue bg-white rounded-full font-bold text-[14px] w-full h-50 hover:text-white hover:bg-campus-blue shadow-none  !border !border-gray-300 border-solid"
 		>
 			{title}
 		</Button>
